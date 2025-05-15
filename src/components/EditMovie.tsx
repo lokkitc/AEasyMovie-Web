fc47de663f7f2178756fde86a7b496781a7a1863
Талимбек Бекзат <lokkitcdev@gmail.com>
2025-05-16 00:18:57 +0500

    Обновлен базовый URL API в компоненте EditMovie: убран префикс '/api' для улучшения взаимодействия с сервером при редактировании фильма.

--- src/components/EditMovie.tsx
+++ src/components/EditMovie.tsx
@@ -93,7 +93,7 @@ export default function EditMovie() {
           ...data,
           duration: Number(data.duration)
         };
-        const response = await api.patch(`/api/movies/${id}`, movieData, {
+        const response = await api.patch(`/movies/${id}`, movieData, {
           headers: {
             'Authorization': `Bearer ${token}`,
             'Content-Type': 'application/json',
