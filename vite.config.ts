036d552c9319e359ab2efb6ff51210385ef6d2c1
Талимбек Бекзат <lokkitcdev@gmail.com>
2025-05-15 23:46:56 +0500

    Обновлен базовый URL API на абсолютный путь и изменена конфигурация прокси Vite для сохранения полного пути в запросах. Это улучшает взаимодействие с сервером и упрощает маршрутизацию запросов.

--- vite.config.ts
+++ vite.config.ts
@@ -17,7 +17,7 @@ export default defineConfig({
         target: 'https://aeasy-movie-server-220072593630.us-central1.run.app',
         changeOrigin: true,
         secure: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+        rewrite: (path) => path,
         configure: (proxy, _options) => {
           proxy.on('error', (err, _req, _res) => {
             console.log('proxy error', err);
